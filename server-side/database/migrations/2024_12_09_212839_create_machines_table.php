918508eb958fda285e127394a5df67eadc458793
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-10 00:29:53 +0300

    Added the machines migration

---
+++ server-side/database/migrations/2024_12_09_212839_create_machines_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('machines', function (Blueprint $table) {
+            $table->id();
+            $table->string('location', 255);
+            $table->decimal('lattitude', 8, 6);
+            $table->decimal('longitude', 9, 6);
+            $table->enum('status', ['active', 'inactive'])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('machines');
+    }
+};
