3a3826334e8468df1462c8b9de013c5b92f3496e
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-10 02:46:28 +0300

    Added the set location route to the api

---
+++ server-side/app/Http/Controllers/ProductsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller{
+
+    public function listProducts(Request $request){
+        
+    }
+}
