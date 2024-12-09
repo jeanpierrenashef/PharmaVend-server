6d9e44dd009ef3d61baee833f7e2e519ea7ab78d
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-10 01:13:27 +0300

    Login and register working

--- server-side/routes/api.php
+++ server-side/routes/api.php
@@ -5,6 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTAuthController;
 use App\Http\Middleware\JwtMiddleware;
 
-
 Route::post("/register", [JWTAuthController::class, "register"]);
 Route::post("/login", [JWTAuthController::class, "login"]);
