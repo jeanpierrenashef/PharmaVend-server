cc0524f0fecc1f3076a4f71dfd8033e8664f4d60
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-11 00:16:45 +0300

    Added the set location which returns the distances to closest vending machine around a user

--- server-side/app/Http/Controllers/LocationController.php
+++ server-side/app/Http/Controllers/LocationController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Machine;
+use Illuminate\Support\Facades\Http;
+
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -20,8 +23,42 @@ class LocationController extends Controller{
         $user->longitude = $request->longitude;
         $user->save();
 
+        $machines = Machine::select('id', 'lattitude', 'longitude')->get();
+
+        $destinations = $machines->map(function ($machine){
+            return "{$machine->lattitude},{$machine->longitude}";
+        })->implode('|');
+
+        $apiKey = "AIzaSyD3EiOtcu7hbFzjSOBIMlFbnU7pXSCq4cw";
+        //$apiKey = env("GOOGLE_API_KEY");
+        $URL = $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=
+                        {$request->lattitude},{$request->longitude}&destinations={$destinations}&mode={$request->mode}&key={$apiKey}";
+
+        $response = Http::get($url); 
+        $data = $response->json();
+
+        if ($data['status'] !== 'OK') {
+            return response()->json(['message' => 'Error fetching distances from Google API'], 500);
+        }
+
+
+        $distances = $data['rows'][0]['elements'];
+        $machineDistances = [];
+
+        foreach ($distances as $index => $distance) {
+            $machineDistances[] = [
+                'machine_id' => $machines[$index]->id,
+                'distance_text' => $distance['distance']['text'], 
+                'distance_value' => $distance['distance']['value'], 
+                'duration_text' => $distance['duration']['text'], 
+                'duration_value' => $distance['duration']['value'], 
+            ];
+        }
+
         return response()->json([
-            "user" => $user
-        ],200);
+            'distances' => $machineDistances
+        ], 200);
     }
+
+
 }
