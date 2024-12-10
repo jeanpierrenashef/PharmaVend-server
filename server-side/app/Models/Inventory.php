131e4a3b89963979c2015663471d9d8a5a198dd5
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-10 05:17:18 +0300

    Updated the inventories migration

--- server-side/app/Models/Inventory.php
+++ server-side/app/Models/Inventory.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 
 class Inventory extends Model{
+    public $incrementing = false;
+    
     protected $fillable = ["machine_id", "product_id", "quantity"];
 }
